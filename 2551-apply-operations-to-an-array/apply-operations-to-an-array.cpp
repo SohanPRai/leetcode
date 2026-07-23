0034e89a18fe2a6146823772f623d56dca3eb1e5
SohanPRai <sohanprai12@gmail.com>
2026-07-23 21:48:16 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (64.04%) - LeetSync

---
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -0,0 +1,31 @@
+    class Solution {
+    public:
+        vector<int> applyOperations(vector<int>& nums) {
+            int k=0;
+            
+            for (int i=0;i<nums.size()-1;i++)
+            {
+                if (nums[i]==nums[i+1])
+                {
+                    nums[i]+=nums[i+1];
+                    nums[i+1]=0;
+                    
+                    }
+            }
+            for (int i=0;i<nums.size();i++){
+                if(nums[i]>0)
+                {
+                nums[k++]=nums[i];
+                
+                    }}
+for(int i=k;i<nums.size();i++){
+    nums[i]=0;
+}
+            
+            
+            
+            return nums;
+        
+        }
+
+    };
